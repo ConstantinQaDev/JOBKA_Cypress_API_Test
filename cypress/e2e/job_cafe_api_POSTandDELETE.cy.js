2564adbd595f5e7ce3d7bb22bb9efe85abd20869
Constantin Colodezni <131937311+ConstantinQaDev@users.noreply.github.com>
2024-04-02 13:37:49 -0700

    Update job_cafe_api_POSTandDELETE.cy.js
    adding API test

--- cypress/e2e/job_cafe_api_POSTandDELETE.cy.js
+++ cypress/e2e/job_cafe_api_POSTandDELETE.cy.js
@@ -3,34 +3,34 @@
 import { data } from "../fixtures/params.json";
 
 describe("Get Jobka Jobs Test", () => {
-  //   let positionBody = {
-  //     position: "SDET",
-  //     company: "mycompanyCOSTA",
-  //     location: "Vancouver",
-  //     seniority: "junior",
-  //     link: "www.linkedin.com",
-  //     description: "some text",
-  //     time: "two hours ago",
-  //     salary: "100k",
-  //     date: "2020-06-06T12:00:00",
-  //   };
+    let positionBody = {
+      position: "SDET",
+      company: "mycompanyCOSTA",
+      location: "Vancouver",
+      seniority: "junior",
+      link: "www.linkedin.com",
+      description: "some text",
+      time: "two hours ago",
+      salary: "100k",
+      date: "2020-06-06T12:00:00",
+    };
 
   let adminKey = "adminadmin";
   let id;
 
-  //   it("should create job listings", () => {
-  //     cy.request({
-  //       method: "POST",
-  //       url: "/create",
-  //       body: positionBody,
-  //       qs: { key: adminKey },
-  //     }).then((response) => {
-  //       console.log(response.body);
-  //       id = response.body.id;
-  //       expect(response.status).equal(201);
-  //       expect(response.body.company).equal("mycompanyCOSTA");
-  //     });
-  //   });
+    it("should create job listings", () => {
+      cy.request({
+        method: "POST",
+        url: "/create",
+        body: positionBody,
+        qs: { key: adminKey },
+      }).then((response) => {
+        console.log(response.body);
+        id = response.body.id;
+        expect(response.status).equal(201);
+        expect(response.body.company).equal("mycompanyCOSTA");
+      });
+    });
 
   it("should create job listings from Fixtures", () => {
     data.forEach((element) => {
